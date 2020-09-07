40934a31445f9744359c7ab62cba38c8910d64fa
Richard South <southy98@gmail.com>
2020-09-07 21:33:14 +0100

    Refactored (& commented out tests)

--- lp-rs/examples/wallpape.rs
+++ lp-rs/examples/wallpape.rs
@@ -1,6 +1,6 @@
 use lp_rs;
 
 fn main() {
-    let thing = lp_rs::download("test.jpg");
+    let thing = lp_rs::download(640, 480, "test.jpg");
     println!("I made a thing: {:?}", thing);
 }
--- lp-rs/src/lib.rs
+++ lp-rs/src/lib.rs
@@ -2,23 +2,13 @@ extern crate reqwest;
 
 use std::fs::File;
 
-pub fn download(file_name: &str) -> Result<bool, String> {
-    let _resp = reqwest::blocking::get("https://picsum.photos/200/300");
-    match _resp {
+pub fn download(width: i32, height: i32, file_name: &str) -> Result<bool, String> {
+    let url = format!("{}/{}", width, height);
+    let resp = reqwest::blocking::get(&format!("https://picsum.photos/{}", url));
+    match resp {
         Ok(mut res) => match File::create(file_name) {
-            Ok(mut file) => {
-                if let Err(e) = ::std::io::copy(&mut res, &mut file) {
-                    eprintln!("Error: {}", e);
-                    Err(e.to_string())
-                } else {
-                    println!("OK!");
-                    Ok(true)
-                }
-            }
-            Err(err) => {
-                eprintln!("Error: {}", err);
-                Err(err.to_string())
-            }
+            Ok(mut file) => save_file(&mut res, &mut file),
+            Err(err) => Err(err.to_string()),
         },
         Err(e) => {
             eprintln!("error.. {}", e);
@@ -27,12 +17,40 @@ pub fn download(file_name: &str) -> Result<bool, String> {
     }
 }
 
-#[cfg(test)]
-mod tests {
-    // use crate::download;
-
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+fn save_file(res: &mut reqwest::blocking::Response, file: &mut File) -> Result<bool, String> {
+    if let Err(e) = ::std::io::copy(res, file) {
+        eprintln!("Error: {}", e);
+        Err(e.to_string())
+    } else {
+        println!("OK!");
+        Ok(true)
     }
 }
+
+// #[cfg(test)]
+// use mockall::{automock, mock, predicate::*};
+// use reqwest::blocking::Response;
+//
+// #[cfg_attr(test, automock)]
+// trait MyTrait {
+//     fn foo(&self, x: u32) -> u32;
+// }
+//
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//
+//     // use crate::download;
+//
+//     #[test]
+//     fn it_works() {
+//         assert_eq!(2 + 2, 4);
+//     }
+//
+//     #[test]
+//     fn mytest() {
+//         let mut mock = MockMyTrait::new();
+//         mock.expect_foo().with(eq(4)).times(1).returning(|x| x + 1);
+//         assert_eq!(5, mock.foo(4));
+//     }
+// }
