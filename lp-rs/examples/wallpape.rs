bd5ed8ac777be33ec30c36a324b519163aa66aab
Richard South <southy98@gmail.com>
2020-09-07 22:46:03 +0100

    Save images with timestamped filenames

--- lp-rs/examples/wallpape.rs
+++ lp-rs/examples/wallpape.rs
@@ -1,6 +1,12 @@
 use lp_rs;
+use std::time::SystemTime;
 
 fn main() {
-    let hmac = lp_rs::download(640, 480, "test.jpg");
+    let time_millis = SystemTime::now()
+        .duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap()
+        .as_millis();
+
+    let hmac = lp_rs::download(640, 480, &format!("test-{}.jpg", time_millis));
     println!("HMAC for the image is: {:?}", hmac);
 }
